95952a4dcd460bb0cb1e617babdbcc6f5d05773d
Mohammed Rahman <mo.rahman610@gmail.com>
2019-01-29 17:47:32 -0500

    fixed input saving

--- TicTacToe/Controllers/HomeController.cs
+++ TicTacToe/Controllers/HomeController.cs
@@ -8,33 +8,84 @@ namespace TicTacToe.Controllers
 {
     public class HomeController : Controller
     {
-        List<string> selectedBoxes = new List<string>();
+        List<string> playerBoxes = new List<string>();
+        List<string> aIBoxes = new List<string>();
 
-        List<List<string>> rowsAndColumns = new List<List<string>>();
-
-
-        List<string> topRow = new List<string>{"A1", "B1", "C1" };
-        List<string> middleRow = new List<string>{ "A2", "B2", "C2" };
-        List<string> bottomRow = new List<string>{ "A3", "B3", "C3" };
-        List<string> leftColumn = new List<string>{"A1", "A2", "A3" };
-        List<string> middleColumn = new List<string>{ "B1", "B2", "B3" };
-        List<string> rightColumn = new List<string>{ "C1", "C2", "C3" };
-        List<string> leftToRigtDiagonal = new List<string>{ "A1", "B2", "C3" };
-        List<string> rigthToLeftDiagonal = new List<string>{ "A3", "B2", "C1" };
-
-        public ActionResult Index(bool newGame, string selectedBox)
+        public ActionResult Index()
         {
-            selectedBoxes.Add(selectedBox);
 
-            if (newGame)
-                selectedBoxes.Clear();
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Index(string selectedBox)
+        {
+
+          /*  if (newGame)
+                playerBoxes.Clear(); */
+
+            playerBoxes.Add(selectedBox);
+
+            ViewBag.A2 = selectedBox;
+
+            List<string>[] rowsAndColumns = new List<string>[8];
+
+            List<string> topRow = new List<string> { "A1", "B1", "C1" };
+            rowsAndColumns[0] = topRow;
+            List<string> middleRow = new List<string> { "A2", "B2", "C2" };
+            rowsAndColumns[1] = middleRow;
+            List<string> bottomRow = new List<string> { "A3", "B3", "C3" };
+            rowsAndColumns[2] = bottomRow;
+            List<string> leftColumn = new List<string> { "A1", "A2", "A3" };
+            rowsAndColumns[3] = leftColumn;
+            List<string> middleColumn = new List<string> { "B1", "B2", "B3" };
+            rowsAndColumns[4] = middleColumn;
+            List<string> rightColumn = new List<string> { "C1", "C2", "C3" };
+            rowsAndColumns[5] = rightColumn;
+            List<string> leftToRigtDiagonal = new List<string> { "A1", "B2", "C3" };
+            rowsAndColumns[6] = leftToRigtDiagonal;
+            List<string> rigthToLeftDiagonal = new List<string> { "A3", "B2", "C1" };
+            rowsAndColumns[7] = rigthToLeftDiagonal;
+
+            bool keepGoing = true;
+
+            foreach (List<string> a in rowsAndColumns)
+            {
+                if (playerBoxes == a)
+                {
+                    keepGoing = false;
+                }
+
+                else if (aIBoxes == a)
+                {
+                    keepGoing = false;
+                }
+            }
+
+            ViewBag.Message = null;
+
+            if (keepGoing == false)
+            {
+                ViewBag.Message = "Congradulations! You've won!";
+            }
+
+
+
+            Random rnd = new Random();
+
+            int aIRowNumber = rnd.Next(0, 2);
+            int aiColumnNumber = rnd.Next(0, 2);
+
+            List<string> aIColumn = rowsAndColumns[aIRowNumber];
+
+            string aIselectedBox = aIColumn[aiColumnNumber];
+            
             return View();
         }
 
         public ActionResult TakeInput(string box)
         {
-            selectedBoxes.Add(box);
+            playerBoxes.Add(box);
 
             return View();
         }
